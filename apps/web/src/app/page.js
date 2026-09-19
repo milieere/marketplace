000fdb7346a3d28124e6d9e21a90e329d78040de
walkeriano <awalkerbarreda@gmail.com>
2026-09-19 18:56:37 +0100

    creación de paginas y componentes

--- apps/web/src/app/page.js
+++ apps/web/src/app/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import Hero from "../components/hero/hero";
 import Header from "../components/header/header";
 
-
 export default function Home() {
   return (
     <>
